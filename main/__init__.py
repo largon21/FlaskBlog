11ce69cfe328b785eb4ac73684e9b09353028bca
largon21 <34896467+largon21@users.noreply.github.com>
2019-07-02 19:54:26 +0200

    db fxed, removed 'unique' attribute in img_file column

--- main/__init__.py
+++ main/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+#login: Artur
+#haslo: Konewka21
 
 app = Flask(__name__)
 
