162842de2f2730b8bc41b6e1f88a5d51edf360aa
largon21 <34896467+largon21@users.noreply.github.com>
2019-07-13 12:25:49 +0200

    update

--- main/routes.py
+++ main/routes.py
@@ -1,3 +1,5 @@
+import secrets, os
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from main import app, db, bcrypt
 from main.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -67,11 +69,28 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     upform = UpdateAccountForm()
     if upform.validate_on_submit():
+        if upform.picture.data:
+            picture_file = save_picture(upform.picture.data)
+            current_user.image_file = picture_file
+
         current_user.username = upform.username.data
         current_user.email = upform.email.data
         db.session.commit()
