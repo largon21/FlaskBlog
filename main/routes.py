92382f063f51e9402200e565d7d71bb1185a07e8
largon21 <34896467+largon21@users.noreply.github.com>
2019-07-17 19:44:43 +0200

    added feture, delete and added buttons for features 'delete' and 'update'

--- main/routes.py
+++ main/routes.py
@@ -110,7 +110,7 @@ def post(post_id):
 
 @app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
-def updatepost(post_id):
+def update_post(post_id):
     post = Post.query.get_or_404(post_id)
     if post.author != current_user:
         abort(403)
@@ -125,3 +125,14 @@ def updatepost(post_id):
         form.title.data = post.title
         form.content.data = post.content
     return render_template('create_post.html', title='Update post', form=form, legend= 'Update Post')
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
